4bd2731088d641af4543993be1fc061a937b7660
Jianyi Cheng <jianyi.cheng@ed.ac.uk>
2025-10-08 11:32:53 +0100

    Added address parameters to show corner cases

--- T2/q2/main.s
+++ T2/q2/main.s
@@ -1,24 +1,23 @@
 .data
 
 A:
-	.word 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-
-B:
-	.word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+	.word 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
 
 	.text
 	.globl main
 
 main:
 
-	la $a0, A        # $a0 = &A[0]
-	la $a1, B        # $a1 = &B[0]
+	la $a3, A        # $a0 = src 
+	addi $a0, $a3, 0        # $a0 = src 
+	addi $a1, $a3, 40        # $a1 = dst
+
 	li $a2, 5        # $a2 = size = 5
 
 	jal func
 
-	la  $a0, B      # base address of B
-	li  $a1, 10     # length = 10
+	la  $a0, A      # base address of A
+	li  $a1, 20     # length = 20
 	jal print_array # call function
 
 	li $v0, 10
